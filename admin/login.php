9808eaffce529e7e040e725ab063d288adddc921
MelvilPE <127743721+MelvilPRO@users.noreply.github.com>
2024-02-26 14:37:01 +0100

    Correction des includes & Rajout CRUD table_user
    - Create index.php
    - Create process.php
    - Create form.php
    - Create delete.php

--- admin/login.php
+++ admin/login.php
@@ -51,7 +51,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <title>Formulaire de Connexion</title>
 </head>
 
@@ -87,7 +87,6 @@
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
