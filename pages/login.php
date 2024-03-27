d326fe13d20d55296b8e60180ca587daea58e98e
MelvilPRO <127743721+MelvilPRO@users.noreply.github.com>
2024-03-27 14:51:00 +0100

    Formulaire de connexion fonctionnel (#13)
    - Rajout du protect sur chaque page protégées
    - Le formulaire de login fonctionne
    - Petite erreur corrigée sur les sessions
    - Créer un fichier protect
    - Créer fichier connect
    - Créer fichier logout
    - Renommer registration_form en register
    - Renommer login_form en login

--- pages/login.php
+++ pages/login.php
@@ -1,10 +1,49 @@
+<?php 
+    require_once $_SERVER["DOCUMENT_ROOT"]."/pages/protected/include/connect.php";
+    require_once $_SERVER["DOCUMENT_ROOT"]."/public/include/global.php";
+
+    $error_message = "";
+    if (isset($_POST["user_mail"]) && isset($_POST["user_password"]))
+    {
+        $user_mail = $_POST["user_mail"];
+        $user_password = $_POST["user_password"];
+
+        $sql = "SELECT * FROM table_user
+        WHERE user_mail = :user_mail";
+        
+        $statement = $db->prepare($sql);
+        $statement->bindParam(":user_mail", $user_mail);
+        $statement->execute();
+        
+        $row_user = $statement->fetch();
+        if (!$row_user)
+        {
+            $error_message = "The account doesn't exist!";
+            goto SHOW_FORM;
+        }
+
+        if (!password_verify($user_password, $row_user["user_password"]))
+        {
+            $error_message = "The password is wrong!";
+            goto SHOW_FORM;
+        }
+        
+        session_start();
+        $_SESSION['user_connected'] = "OK";
+        $_SESSION['user_name'] = $row_user['user_name'];
+        header("location: protected/contact.php");
+        exit();
+        SHOW_FORM:
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>loginForm</title>
+    <title>Login to CarPool</title>
     <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css" />
     <link rel="stylesheet" href="/css/style.css" />
     <link rel="stylesheet" href="/css/variable_bootstrap.css" />
@@ -27,16 +66,16 @@
             <div class="row">
                 <div class="d-flex justify-content-center align-items-center text-white">
                     <div class="form-login-items">
-                        <form action="sender.php" class="form-login-labels" method="POST">
+                        <form action="login.php" class="form-login-labels" method="POST">
                             <h1 class="text-white text-center mt-3 m-3">Connexion au compte</h1>
                             <div class="form-group">
                                 <label for="user_mail">Adresse mail</label>
-                                <input type="text" class="form-control form-control-lg" id="user_mail"
+                                <input type="text" class="form-control form-control-lg" id="user_mail" name="user_mail"
                                     placeholder="Adresse mail">
                             </div>
                             <div class="form-group">
                                 <label for="user_password">Mot de passe</label>
-                                <input type="text" class="form-control form-control-lg" id="user_password"
+                                <input type="password" class="form-control form-control-lg" id="user_password" name="user_password"
                                     placeholder="Mot de passe">
                             </div>
                             <div class="mb-3">
